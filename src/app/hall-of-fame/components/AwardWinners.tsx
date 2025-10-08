017fd6100fb29bf56e8f4a86d9ad48e26fd7d398
ega <202210027@fit.edu.ph>
2025-10-08 14:47:54 +0800

    style: add cards and polish initial layout for `AwardWinners` section

--- src/app/hall-of-fame/components/AwardWinners.tsx
+++ src/app/hall-of-fame/components/AwardWinners.tsx
@@ -1,12 +1,31 @@
 export function AwardWinners() {
-
-    
   return (
-    <section className=" flex flex-col items-center gap-y-10 justify-center min-h-[180vh] bg-cyan-400">
-        
-      <div className="bg-white w-2/3 flex-1 flex items-center justify-center mt-10">Award 1</div>
-      <div className="bg-white w-2/3 flex-1 flex items-center justify-center">Award 2</div>
-      <div className="bg-white w-2/3 flex-1 flex items-center justify-center mb-10">Award 3</div>
+    <section className="flex flex-col items-center gap-6 bg-cyan-400 p-4 pt-10 md:gap-8 md:p-6 lg:gap-10 lg:p-8">
+      
+      {/* award 1 */}
+      <h1 className="text-xl font-bold md:text-2xl lg:text-3xl">Award Title 1</h1>
+      <div className="flex w-full flex-col items-center justify-center gap-4 bg-white p-4 md:w-4/5 md:flex-row lg:w-2/3">
+        <div className="flex h-100 w-full items-center justify-center bg-lime-400  md:w-1/3 ">1</div>
+        <div className="flex h-100 w-full items-center justify-center bg-red-400 md:w-1/3 ">2</div>
+        <div className="flex h-100 w-full items-center justify-center bg-fuchsia-400 md:w-1/3">3</div>
+      </div>
+
+      {/* award 2 */}
+      <h1 className="text-xl font-bold md:text-2xl lg:text-3xl">Award Title 2</h1>
+      <div className="flex w-full flex-col items-center justify-center gap-4 bg-white p-4 md:w-4/5 md:flex-row lg:w-2/3">
+        <div className="flex h-100 w-full items-center justify-center bg-lime-400  md:w-1/3 ">1</div>
+        <div className="flex h-100 w-full items-center justify-center bg-red-400 md:w-1/3 ">2</div>
+        <div className="flex h-100 w-full items-center justify-center bg-fuchsia-400 md:w-1/3">3</div>
+      </div>
+
+      {/* award 3 */}
+      <h1 className="text-xl font-bold md:text-2xl lg:text-3xl">Award Title 3</h1>
+      <div className="flex w-full flex-col items-center justify-center gap-4 bg-white p-4 md:w-4/5 md:flex-row lg:w-2/3">
+        <div className="flex h-100 w-full items-center justify-center bg-lime-400  md:w-1/3 ">1</div>
+        <div className="flex h-100 w-full items-center justify-center bg-red-400 md:w-1/3 ">2</div>
+        <div className="flex h-100 w-full items-center justify-center bg-fuchsia-400 md:w-1/3">3</div>
+      </div>
+
     </section>
   );
 }
