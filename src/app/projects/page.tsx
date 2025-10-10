34a62c7e3b11f1bd63d85c1ad783da067eb566b3
HardinKylle <mariushardin@gmail.com>
2025-10-10 22:13:05 +0800

    create: initial layout setup for project page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+import ProjectsHero from "./components/ProjectsHero";
+
+export default function ProjectsPage() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen p-8 bg-[#0f001e] text-white">
+      <ProjectsHero />
+    </main>
+  );
+}
