4bb8f82e8d3396e0dee3cf7f53f034ba23d4d170
HardinKylle <mariushardin@gmail.com>
2025-10-13 01:04:49 +0800

    test: page not found bug in local repository

--- src/app/projects/components/ProjectsGrid.tsx
+++ src/app/projects/components/ProjectsGrid.tsx
@@ -4,6 +4,7 @@ import ProjectsCard from "./ProjectsCard";
 export default function ProjectsGrid() {
   const projectCount = Array.from({ length: 9 });
 
+  {/* 3x3 Grid Layout of cards */}
   return (
     <section className="w-full max-w-[1200px] py-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto">
       {projectCount.map((_, index) => (
