e96b2ce76e52fdf370c6e6449e05777310f7352d
ega <202210027@fit.edu.ph>
2025-10-13 21:08:26 +0800

    feat(hall-of-fame): integrate reusable hero section

--- src/app/hall-of-fame/components/HallOfFameHero.tsx
+++
@@ -1,9 +0,0 @@
-export function HOFHero() {
-  return (
-    <>
-      <section className="flex h-screen items-center justify-center bg-amber-400 p-4">
-        <div>HALL OF FAME: HERO</div>
-      </section>
-    </>
-  )
-}
---
+++ src/app/hall-of-fame/ui/ProjectsHero.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image"
+
+// accepts pageTitle and pageDesc as props
+export function ProjectsHero({ pageTitle, pageDesc }: { pageTitle: string; pageDesc: string }) {
+  return (
+    <>
+      <section className="flex h-[110vh] flex-col">
+        <div className="flex h-[65%] flex-col items-center justify-center bg-gradient-to-t from-[#AF05C5] from-[0%] via-[#480CA8] via-[20%] to-black to-[80%] text-white">
+          <div className="mx-auto flex max-w-6xl translate-y-10 flex-col items-center justify-center px-4 text-center">
+            <h1 className="font-regular mb-4 text-4xl leading-[100px] md:text-8xl lg:text-[128px]">
+              {pageTitle}
+            </h1>
+            <p className="text-[14px] leading-[20px] font-light md:text-base lg:text-lg">
+              {pageDesc}
+            </p>
+          </div>
+        </div>
+        <div className="h-[35%] bg-gradient-to-t from-black from-[15%] via-[#480CA8] via-[85%] to-[#C39EFF] to-[100%]">
+          <div className="relative h-full w-full -translate-y-0.5">
+            <Image
+              src="/HOF/vector.svg"
+              alt="Projects Hero"
+              fill
+              className="object-cover lg:object-fill"
+              quality={100}
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
