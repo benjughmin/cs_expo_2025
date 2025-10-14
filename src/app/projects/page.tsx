82f33a20e240648407ef327f3f5300563104bced
HardinKylle <mariushardin@gmail.com>
2025-10-14 22:56:50 +0800

    feat: update ProjectsHero component with title and description

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,14 @@
-import ProjectsHero from "@/components/projects/ProjectsHero";
+import {ProjectsHero} from "@/app/hall-of-fame/ui/ProjectsHero";
 import CategoryFilters from "@/components/projects/CategoryFilters";
 import ProjectsGrid from "@/components/projects/ProjectsGrid";
 
 export default function ProjectsPage() {
   return (
-    <main className="flex flex-col items-center justify-start min-h-screen p-8 bg-black text-white overflow-y-auto">
-      <ProjectsHero />
+    <main className="bg-black">
+      <ProjectsHero
+        pageTitle="Hall Of Fame"
+        pageDesc="Join the first day of CS EXPO, as bright fourth-year students present their innovative theses with topics regarding data analytics, health, and natural language processing. Under the data analytics category are groups Agatha, LMNTRX, and Strawhats. For the health category, Code Geass, Hurtz, Lorem Ipsum, and Samsan. Lastly, for the natural language processing category are the thesis groups Codex, Ctrl Alt Delete, EnSys, Jathro's Matrix, and LMDIFY."
+      />
       <CategoryFilters />
       <ProjectsGrid />
     </main>
